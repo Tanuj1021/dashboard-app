b012de3ed7e88745a1297f7972596110090be960
Tanuj1021 <54186010+Tanuj1021@users.noreply.github.com>
2023-08-17 10:10:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainScreenComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
